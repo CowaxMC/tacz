994fd542cd43a6bc64063298715039f3d4527137
nikita1805200 <153413406+nikita1805200@users.noreply.github.com>
2025-09-11 21:18:42 +0300

    1.0.2
    tipo balans

---
+++ bronya_SI.js
@@ -0,0 +1,40 @@
+ItemEvents.modification(event => {
+  event.modify('survival_instinct:green_recluit_armor_helmet', item => {
+    item.armorProtection = 1
+  })
+})
+ItemEvents.modification(event => {
+  event.modify('survival_instinct:green_recluit_armor_chestplate', item => {
+    item.armorProtection = 4
+  })
+})
+ItemEvents.modification(event => {
+  event.modify('survival_instinct:green_recluit_armor_leggings', item => {
+    item.armorProtection = 4
+  })
+})
+ItemEvents.modification(event => {
+  event.modify('survival_instinct:green_recluit_armor_boots', item => {
+    item.armorProtection = 2
+  })
+})
+ItemEvents.modification(event => {
+  event.modify('survival_instinct:green_rockie_armor_helmet', item => {
+    item.armorProtection = 3
+  })
+})
+ItemEvents.modification(event => {
+  event.modify('survival_instinct:green_rockie_armor_chestplate', item => {
+    item.armorProtection = 6
+  })
+})
+ItemEvents.modification(event => {
+  event.modify('survival_instinct:green_rockie_armor_leggings', item => {
+    item.armorProtection = 4
+  })
+})
+ItemEvents.modification(event => {
+  event.modify('survival_instinct:green_rockie_armor_boots', item => {
+    item.armorProtection = 2
+  })
+})
